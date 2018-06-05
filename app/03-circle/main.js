c508ba4db0d4c8aa4a2fd548add60b0554aad3c8
Dominique SZCZYGIEL <tursyop@yahoo.fr>
2018-06-05 09:53:38 +0200

    ajout d'un point

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -12,15 +12,29 @@ class Circle {
 </svg>
     `;
     }
+
+    setPoint(angle) {
+        // Attention, lorsqu'on travaille avec du svg il faut forcément travailler dans un namespace
+        const svgns = "http://www.w3.org/2000/svg";
+        const svg = document.querySelector('svg');
+        // createElement de Javascript est à remplacer par createElementNS, à cause du namespace
+        const point = document.createElementNS(svgns,'circle');
+        point.setAttribute('cx', 1.5*this.radius);
+        point.setAttribute('cy', 1.5*this.radius);
+        point.setAttribute('r', 10);
+        point.setAttribute('fill', '#000');
+        point.setAttribute('stroke', '#000');
+        point.setAttribute('stroke-width', '2');
+        svg.appendChild(point);
+    }
 }
 
-function makeCircle() {
-} 
 
 function main(){
         console.log('Tracé du cercle')
         const c = new Circle(300);
         c.draw();
+        c.setPoint(10);
    }
     
 main();
