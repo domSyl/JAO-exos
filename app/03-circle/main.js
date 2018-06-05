275f21bcf8cfe52a77e418532561104f0c790821
Dominique SZCZYGIEL <tursyop@yahoo.fr>
2018-06-05 16:04:00 +0200

    en iife

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,11 +1,10 @@
 const sleep = d => new Promise(r => setTimeout(r, d));
 
-async function main() {
+(async function main() {
+    'use strict'
     for (let i = 0; i < 100; i++) {
         const m = new Multiply(i, 20);
         m.draw();
         await sleep(16);
     }
-}
-
-main();
+})();
