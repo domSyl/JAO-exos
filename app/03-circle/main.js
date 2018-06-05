778412e295c7598dfe42bd364691060dd15ae762
Dominique SZCZYGIEL <tursyop@yahoo.fr>
2018-06-05 10:29:16 +0200

    Sortie du namespace en constante globale

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,3 +1,6 @@
+// Attention, lorsqu'on travaille avec du svg il faut forcément travailler dans un namespace
+const SVGNS = "http://www.w3.org/2000/svg";
+
 class Circle {
     constructor(radius) {
         this.radius = radius;
@@ -17,11 +20,9 @@ class Circle {
 
     setPoint(angle) {
         const a = angle * 2 * Math.PI / 360 ; 
-        // Attention, lorsqu'on travaille avec du svg il faut forcément travailler dans un namespace
-        const svgns = "http://www.w3.org/2000/svg";
         const svg = document.querySelector('svg');
         // createElement de Javascript est à remplacer par createElementNS, à cause du namespace
-        const point = document.createElementNS(svgns,'circle');
+        const point = document.createElementNS(SVGNS,'circle');
         point.setAttribute('cx', this.cx + this.radius * Math.cos(a));
         point.setAttribute('cy', this.cy - this.radius * Math.sin(a));
         point.setAttribute('r', 3);
@@ -34,9 +35,8 @@ class Circle {
     setLine(angle1, angle2) {
         const a1 = angle1 * 2 * Math.PI / 360 ; 
         const a2 = angle2 * 2 * Math.PI / 360 ; 
-        const svgns = "http://www.w3.org/2000/svg";
         const svg = document.querySelector('svg');
-        const line = document.createElementNS(svgns,'line');
+        const line = document.createElementNS(SVGNS,'line');
         line.setAttribute('x1', this.cx + this.radius * Math.cos(a1));
         line.setAttribute('y1', this.cy - this.radius * Math.sin(a1));
         line.setAttribute('x2', this.cx + this.radius * Math.cos(a2));
