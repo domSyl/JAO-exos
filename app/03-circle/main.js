ec8a70cd6bd3512f4f9bb409f09f8dd4f2c27dd3
Dominique SZCZYGIEL <tursyop@yahoo.fr>
2018-06-05 15:29:28 +0200

    Avec promise au lieu de setTimeout dans le main()

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,11 +1,18 @@
-function main(){
-        for (let i = 0; i < 1000; i++){
-            setTimeout(() => {
-                const m = new Multiply(i, 20);
-                m.draw();
-            }, 16*i)
-            
-        }
-   }
-    
+function sleep(delay) {
+    //En simplification de code, on peut écrire : return new Promise(resolve => {instructions})
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, delay);
+    });
+}
+
+async function main() {
+    for (let i = 0; i < 1000; i++) {
+        const m = new Multiply(i, 20);
+        m.draw();
+        await sleep(16);
+    }
+}
+
 main();
