11d88379de993ed9b1e0f480e5a8b62afaf9d339
Dominique SZCZYGIEL <tursyop@yahoo.fr>
2018-06-05 09:31:58 +0200

    classe

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,14 +1,23 @@
-function makeCircle() {
-    const div = document.querySelector('div');
+class Circle {
+    constructor() {}
+
+    draw(){
+        const div = document.querySelector('div');
     div.innerHTML = `
-    <svg width="600" height="600">
-        <circle cx="300" cy="300" r="200" stroke="#000" stroke-width="2" fill="transparent" />
-    </svg>
+<svg width="600" height="600">
+    <circle cx="300" cy="300" r="200" stroke="#000" stroke-width="2" fill="transparent" />
+</svg>
     `;
+    }
+}
+
+function makeCircle() {
 } 
 
 function main(){
-       makeCircle();
+        console.log('Tracé du cercle')
+        const c = new Circle();
+        c.draw();
    }
     
 main();
