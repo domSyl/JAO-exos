dfd97f95e2cb51039ed1106f877017a94a61fc80
Dominique SZCZYGIEL <tursyop@yahoo.fr>
2018-06-05 10:03:34 +0200

    placer les points sur le cercle

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,27 +1,30 @@
 class Circle {
     constructor(radius) {
         this.radius = radius;
+        this.cx = 1.5 * radius;
+        this.cy = 1.5 * radius;
     }
 
     draw(){
         const div = document.querySelector('div');
         const r = this.radius;
     div.innerHTML = `
-<svg width="${3*r}" height="${3*r}">
-    <circle cx="${1.5*r}" cy="${1.5*r}" r="${r}" stroke="#000" stroke-width="2" fill="transparent" />
+<svg width="${3 * r}" height="${3 * r}">
+    <circle cx="${this.cx}" cy="${this.cy}" r="${r}" stroke="#000" stroke-width="2" fill="transparent" />
 </svg>
     `;
     }
 
     setPoint(angle) {
+        const a = angle * 2 * Math.PI / 360 ; 
         // Attention, lorsqu'on travaille avec du svg il faut forcément travailler dans un namespace
         const svgns = "http://www.w3.org/2000/svg";
         const svg = document.querySelector('svg');
         // createElement de Javascript est à remplacer par createElementNS, à cause du namespace
         const point = document.createElementNS(svgns,'circle');
-        point.setAttribute('cx', 1.5*this.radius);
-        point.setAttribute('cy', 1.5*this.radius);
-        point.setAttribute('r', 10);
+        point.setAttribute('cx', this.cx + this.radius * Math.cos(a));
+        point.setAttribute('cy', this.cy - this.radius * Math.sin(a));
+        point.setAttribute('r', 3);
         point.setAttribute('fill', '#000');
         point.setAttribute('stroke', '#000');
         point.setAttribute('stroke-width', '2');
@@ -34,7 +37,9 @@ function main(){
         console.log('Tracé du cercle')
         const c = new Circle(300);
         c.draw();
-        c.setPoint(10);
+        c.setPoint(30);
+        c.setPoint(60);
+        c.setPoint(90);
    }
     
 main();
